0da21eff1860f18547aa69a33aef4a1092c5e0d4
uc <an@ucui-MacBookPro.local>
2026-04-12 01:14:53 +0900

    SCRUM-9 사용자 설정

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("hello");
         System.out.print("hello2");
-
+        System.out.print("hello3");
 
     }
 
